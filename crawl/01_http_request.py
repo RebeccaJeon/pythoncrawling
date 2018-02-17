fdf99e48deb879b5bc35bb5a26d94b80972a3b44
kyeongrok <oceanfog1@gmail.com>
2018-02-17 14:28:37 +0900

    code바꾸고 필요한 필드 뽑기

--- crawl/01_http_request.py
+++ crawl/01_http_request.py
@@ -1,8 +1,16 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import json
 
-url = "http://companyinfo.stock.naver.com/v1/company/cF3002.aspx?cmp_cd=068270&frq=0&rpt=0&finGubun=MAIN&frqTyp=0&cn="
+code = "005930"
+url = "http://companyinfo.stock.naver.com/v1/company/cF3002.aspx?cmp_cd={}&frq=0&rpt=0&finGubun=MAIN&frqTyp=0&cn=".format(code)
 html = urlopen(url)
 bsObj = BeautifulSoup(html, "html.parser")
 
-print(bsObj)
+#string to json object
+jsonObject = json.loads(str(bsObj))
+data = jsonObject['DATA']
+
+for item in data:
+    result = "{},{},{},{},{},{}".format(item['ACC_NM'], item['DATA1'], item['DATA2'], item['DATA3'], item['DATA4'], item['DATA5'])
+    print(result)
